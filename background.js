115bf113d1a8a91bd78b6a52a4315a4fea4fce3c
Luke Pisano <79476828+Luke-Pisano@users.noreply.github.com>
2022-09-08 10:15:10 -0600

    1.29
    Added support for APA
    Format now savable in user settings

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
-chrome.browserAction.onClicked.addListener(() => chrome.tabs.create({
-    url: "https://www.imasse.com/"
-})), chrome.runtime.onInstalled.addListener(() => chrome.tabs.create({
-    url: "https://www.imasse.com/welcome"
-}));
+chrome.runtime.onInstalled.addListener(async (tab) => {
+    chrome.contextMenus.create({
+        title: "Search Imasse",
+        contexts: ["selection"],
+        id: "imasseContextMenu"
+    });
+});
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+    let url = new URL(`https://www.imasse.com/search`)
+    url.searchParams.set('q', item.selectionText)
+    chrome.tabs.create({
+        url: url.href,
+        index: tab.index + 1
+    });
+});
+chrome.runtime.onInstalled.addListener(async (tab) => {
+    chrome.tabs.create({
+        url: "https://www.imasse.com/welcome"
+    });
+});
